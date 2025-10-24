565082b7b8087926b457199078dc0dc3c22ac981
osaeid rony <osaeidfire@gmail.com>
2025-10-24 19:20:29 +0100

    reverse

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ for i in range(1, row+1):
     
 name = "Osaeid"
 
-for i in range(1, 6):
-    for j in range(1, i):
+for i in range(0, 7):
+    for j in range(0, i):
         print(name[j], end=" ")
+    print()
+    
+length = len(name)
+
+for i in range(length):
+    for j in range(length-1-i, 0, -1):
+        print(end="  ")
+    for k in range(i+1, 0, -1):
+        print(name[k-1], end=" ")
     print()
